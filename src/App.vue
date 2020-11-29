7bbc197c3fec72457e5b63750707056d55f13355
Knight174 <425302118@qq.com>
2020-11-29 20:36:32 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Hello world!</h1>
+  <Cpc msg="cpc" />
+</template>
+
+<script>
+import Cpc from './components/Cpc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cpc
+  }
+}
+</script>
---
+++ src/components/Cpc.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi, {{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'cpc',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
